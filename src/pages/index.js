a98c723e9aabb8173d903044d4a186dd9464a6b3
Katie Hughes <katelhughes93@gmail.com>
2020-09-13 16:32:46 -0700

    No one is using a description

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,7 +63,6 @@ export const pageQuery = graphql`
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
             title
-            description
           }
         }
       }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -94,7 +94,6 @@ export const pageQuery = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
-        description
       }
     }
   }
