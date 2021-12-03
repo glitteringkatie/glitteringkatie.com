20a2ae6cd9512eb649a2520fe907d17d33c9e8ac
Katie Hughes <katelhughes93@gmail.com>
2021-12-02 22:20:32 -0800

    Clean up formatting just a tiiiiiiiiiny bit

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -3,11 +3,14 @@ import html from 'remark-html'
 import remarkGfm from 'remark-gfm';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(remarkGfm).use(html).use(() => {
-    return function transformer(tree) {
-      console.log(tree);
-    }
-  }).process(markdown)
+  const result = await remark()
+    .use(remarkGfm)
+    .use(html)
+    .use(() => {
+      return function transformer(tree) {
+        console.log(tree);
+      }
+    }).process(markdown)
 
   console.log(result);
   return result.toString()
