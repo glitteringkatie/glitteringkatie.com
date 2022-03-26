c0c34bfebfe31cbf99c88b06ef325d2faa3a9f18
Katie Hughes <katelhughes93@gmail.com>
2022-03-26 16:59:13 -0700

    add a bold fontweight for work sans

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
             href="https://fonts.gstatic.com"
             crossOrigin="true"></link>
           <link
-            href="https://fonts.googleapis.com/css2?family=Fira+Mono&family=Source+Serif+Pro:ital,wght@0,600;1,400&family=Work+Sans:wght@200;400&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Fira+Mono&family=Source+Serif+Pro:ital,wght@0,600;1,400&family=Work+Sans:wght@200;400;600&display=swap"
             rel="stylesheet"
           />
         </Head>
